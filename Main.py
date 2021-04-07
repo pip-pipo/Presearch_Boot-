d3aaa0d4d1d14bc2db449aae8067aa092d7f84f7
pip-pipo <codemi692@gmail>
2021-04-07 00:49:27 -1100

    :fire: readme.md added

--- Main.py
+++ Main.py
@@ -15,7 +15,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-import SearchWithKeyWords
 import random
 import time
 from dotenv import load_dotenv, find_dotenv
@@ -32,13 +31,21 @@ class Presearch:
         chrome_options.add_argument("--user-data-dir=/tmp/chrome2/")
         # chrome_options.add_argument("--user-data-dir=chrome-data")
         chrome_options.add_argument("--start-maximized")
-        self.driver = webdriver.Chrome(executable_path='./chromedriver.exe',options=chrome_options)
-        #is browser new tab Open 
+        self.driver = webdriver.Chrome(
+            executable_path='./chromedriver.exe', options=chrome_options)
+        # is browser new tab Open
         self.isNewTabOpen = False
         # count How Many Times Searched
         self.count = 0
         # keywords list
-        self.keyWords = ["dhaka","Islamabad","Bangladesh","Pakistan","karachi","ISS","WEB DEVELOPMENT"]
+        self.keyWords = ['dhaka',
+                         'Islamabad', 'Bangladesh',
+                         'Pakista','karachi',
+                         'ISS', 'WEB DEVELOPMENT', 'amazing', 'beautiful', 'adventure', 'photography', 'nofilter',
+                         'newyork', 'artsy', 'alumni', 'lion', 'best', 'fun', 'happy',
+                         'art', 'funny', 'me', 'followme', 'follow', 'cinematography', 'cinema',
+                         'love', 'instagood', 'instagood', 'followme', 'fashion', 'sun', 'scruffy',
+                         'street', 'canon', 'beauty', 'studio', 'pretty', 'vintage', 'fierce']
 
     def openPresearch(self):
         driver = self.driver
@@ -47,13 +54,13 @@ class Presearch:
         driver.maximize_window()
 
         # validate is user already logged
-        isUserLogged = driver.find_element(By.XPATH,"//*[@id='user-menu-toggle']")
+        isUserLogged = driver.find_element(
+            By.XPATH, "//*[@id='user-menu-toggle']")
         # print(not isUserLogged) False means user already not logged
         if not isUserLogged:
             self.MakeRegisterOrLogIn()
         else:
             self.runProcessInfiniteTimes()
-            
 
     def MakeRegisterOrLogIn(self):
 
@@ -61,60 +68,57 @@ class Presearch:
         # user EMAIL AND PASSWORD FROM .env file
         Email = os.environ.get("EMAIL")
         Password = os.environ.get("PASSWORD")
-       
+
         # click loginOrRegisterButton
         time.sleep(4)
         elm = driver.find_element(
             By.XPATH, "/html/body/div[1]/div[1]/header/nav/div/div[2]/ul/li[5]/a")
         elm.click()
 
-
         # fill Email Input
         time.sleep(4)
         emailInput = driver.find_element(
-                By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[3]/div[1]/form/div[1]/input")
+            By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[3]/div[1]/form/div[1]/input")
         emailInput.clear()
         emailInput.send_keys(Email)
 
         # fill password Input
         time.sleep(4)
         passwordInput = driver.find_element(
-                By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[3]/div[1]/form/div[2]/div/input")
+            By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[3]/div[1]/form/div[2]/div/input")
         passwordInput.clear()
         passwordInput.send_keys(Password)
 
         # captha Validation Manual
-        
+
         print("Please fill the captha manually")
-           
+
         WaitUntillUserNotClickToLoginButton = driver.find_element(
-                By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[3]/div[1]/form/div[3]/div[3]/button")
+            By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[3]/div[1]/form/div[3]/div[3]/button")
 
         WebDriverWait(driver, timeout=500).until(
-                EC.staleness_of(WaitUntillUserNotClickToLoginButton))
-        
+            EC.staleness_of(WaitUntillUserNotClickToLoginButton))
+
         time.sleep(4)
         self.runProcessInfiniteTimes()
-       
-
 
     def SearchWithKeyWords(self):
         for i in range(10):
             self.OpenNewTab()
-
-
+        time.sleep(4)
+        self.driver.close()
 
     def OpenNewTab(self):
-        self.isNewTabOpen==True
-        self.count+=1
+        self.isNewTabOpen == True
+        self.count += 1
         driver = self.driver
-        keyWords =self.keyWords
+        keyWords = self.keyWords
         time.sleep(4)
 
         driver.get("https://presearch.org")
 
         # click the search input element
-        searchInput = driver.find_element(By.XPATH,"//*[@id='search']")
+        searchInput = driver.find_element(By.XPATH, "//*[@id='search']")
         searchInput.clear()
 
         # use random keywords
@@ -126,19 +130,20 @@ class Presearch:
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
         time.sleep(4)
 
-        # make like human 
+        # make like human
 
         # goto search releted next page
-        clickGotoPage1 = driver.find_element(By.XPATH,"/html/body/div/div/div[3]/div[3]/div[1]/a")
+        clickGotoPage1 = driver.find_element(
+            By.XPATH, "/html/body/div/div/div[3]/div[3]/div[1]/a")
         time.sleep(4)
         clickGotoPage1.click()
 
         time.sleep(8)
         driver.get("https://presearch.org")
 
-        # repeate Task 
+        # repeate Task
         # Todo ::: Make SomeThing InterActive
-        searchInput = driver.find_element(By.XPATH,"//*[@id='search']")
+        searchInput = driver.find_element(By.XPATH, "//*[@id='search']")
         searchInput.clear()
         time.sleep(2)
         searchInput.send_keys(random.choice(keyWords))
@@ -148,16 +153,16 @@ class Presearch:
         # ))
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
         time.sleep(4)
-        clickGotoPage1 = driver.find_element(By.XPATH,"/html/body/div/div/div[3]/div[3]/div[1]/a")
+        clickGotoPage1 = driver.find_element(
+            By.XPATH, "/html/body/div/div/div[3]/div[3]/div[1]/a")
         time.sleep(2)
         clickGotoPage1.click()
         # end Todo
 
-       
     def runProcessInfiniteTimes(self):
         # use loop for making infinite
         self.SearchWithKeyWords()
-   
+
 
 runPresearch = Presearch()
 runPresearch.openPresearch()
---
+++ README.md
@@ -0,0 +1,88 @@
+<div align="center" id="top"> 
+  <img src="./.github/app.gif" alt="C++" />
+
+  &#xa0;
+
+  <!-- <a href="https://c++.netlify.app">Demo</a> -->
+</div>
+
+<h1 align="center">C++</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/pip-pipo/Presearch_Boot-?color=56BEB8">
+
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/pip-pipo/Presearch_Boot-?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/pip-pipo/Presearch_Boot-?color=56BEB8">
+
+  <img alt="Github issues" src="https://img.shields.io/github/issues/pip-pipo/Presearch_Boot-?color=56BEB8" />
+
+  <img alt="Github forks" src="https://img.shields.io/github/forks/pip-pipo/Presearch_Boot-?color=56BEB8" />
+
+  <img alt="Github stars" src="https://img.shields.io/github/stars/pip-pipo/Presearch_Boot-?color=56BEB8" />
+</p>
+
+
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/pip-pipo" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About ##
+
+Presearch_Boot😍 https://presearch.org search engine free Boot 100% with Presearch Gude lines Work..🤩🤘🏻 Give a Star 
+
+## :sparkles: Features ##
+
+:heavy_check_mark: auto search;\
+:heavy_check_mark: auto login;\
+:heavy_check_mark: auto random keywords search;
+
+## :rocket: Technologies ##
+
+The following tools were used in this project:
+
+- python
+
+
+
+
+## :checkered_flag: Starting ##
+
+```bash
+
+# Clone this project
+$ git clone https://github.com/pip-pipo/Presearch_Boot-
+
+# install requirements.txt
+$ pip install -r requirements.txt
+
+# Run the project
+$ python Main.py
+
+# for unix or linux
+
+$ python3 Main.py
+
+
+```
+
+## :memo: License ##
+
+This project is under license from MIT. For more details, see the [LICENSE](LICENSE.md) file.
+
+
+Made with :heart: by <a href="https://github.com/pip-pipo" target="_blank">Morsalin</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
